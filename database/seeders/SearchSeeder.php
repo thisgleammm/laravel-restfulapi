b2ed513d9a62503fdf2827de179875b118ac11ff
thisgleammm <thisgleam@gmail.com>
2026-02-09 11:34:11 +0700

    Search Contact API

---
+++ database/seeders/SearchSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Contact;
+use App\Models\User;
+
+class SearchSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::where('username', 'test')->first();
+        for ($i = 0; $i < 20; $i++) {
+            Contact::create([
+                'first_name' => 'first ' . $i,
+                'last_name' => 'last ' . $i,
+                'email' => 'test ' . $i . '@gmail.com',
+                'phone' => '1111 ' . $i,
+                'user_id' => $user->id,
+            ]);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,8 @@ Route::middleware(ApiAuthMiddleware::class)->group(function() {
     Route::patch('/users/current', [UserController::class, 'update']);
     Route::delete('/users/logout', [UserController::class, 'logout']);
 
-    Route::post('/contacts', [ContactController::class, 'create']);
+    Route::post('/contacts', action: [ContactController::class, 'create']);
+    Route::get('/contacts', action: [ContactController::class, 'search']);
     Route::get('/contacts/{id}', [ContactController::class, 'get'])->where('id', '[0-9]+');
     Route::put('/contacts/{id}', [ContactController::class, 'update'])->where('id', '[0-9]+');
     Route::delete('/contacts/{id}', [ContactController::class, 'delete'])->where('id', '[0-9]+');
