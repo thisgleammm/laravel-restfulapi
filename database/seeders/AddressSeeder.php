eaea12ed8b88a61cd5c1292789cc1943e3b30d2b
thisgleammm <thisgleam@gmail.com>
2026-02-09 13:37:24 +0700

    Get Address API

--- database/seeders/AddressSeeder.php
+++ database/seeders/AddressSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Address;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Contact;
 
 class AddressSeeder extends Seeder
 {
@@ -12,6 +14,14 @@ class AddressSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $contact = Contact::query()->limit(1)->first();
+        Address::create([
+            'contact_id' => $contact->id,
+            'street' => 'test',
+            'city' => 'test',
+            'province' => 'test',
+            'country' => 'test',
+            'postal_code' => '1111',
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -13,11 +13,18 @@ Route::middleware(ApiAuthMiddleware::class)->group(function() {
 
     Route::post('/contacts', action: [ContactController::class, 'create']);
     Route::get('/contacts', action: [ContactController::class, 'search']);
-    Route::get('/contacts/{id}', [ContactController::class, 'get'])->where('id', '[0-9]+');
-    Route::put('/contacts/{id}', [ContactController::class, 'update'])->where('id', '[0-9]+');
-    Route::delete('/contacts/{id}', [ContactController::class, 'delete'])->where('id', '[0-9]+');
+    Route::get('/contacts/{id}', [ContactController::class, 'get'])
+        ->where('id', '[0-9]+');
+    Route::put('/contacts/{id}', [ContactController::class, 'update'])
+        ->where('id', '[0-9]+');
+    Route::delete('/contacts/{id}', [ContactController::class, 'delete'])
+        ->where('id', '[0-9]+');
 
-    Route::post('/contacts/{idContact}/addresses', [AddressController::class, 'create'])->where('idContact', '[0-9]+');
+    Route::post('/contacts/{idContact}/addresses', [AddressController::class, 'create'])
+        ->where('idContact', '[0-9]+');
+    Route::get('/contacts/{idContact}/addresses/{idAddress}', [AddressController::class, 'get'])
+        ->where('idContact', '[0-9]+')
+        ->where('idAddress', '[0-9]+');
 });
 Route::post('/users', [UserController::class, 'register']);
 Route::post('/users/login', [UserController::class, 'login']);
